c986221e0d05b26d9414e9db28d18e4c202f2f50
basit-devBE <basitmohammed362@gmail.com>
2023-11-02 13:50:14 +0000

    hello

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import add_0
-    a = int(1)
-    b = int(2)
+    from add_0 import add
 
-    print(f"{a} + {b} = {add_0.add(a, b)}")
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
