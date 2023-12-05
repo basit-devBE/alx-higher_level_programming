c3a8ef5e392bbbea4ccad9f55c253c7df907be3a
baasit <basitmohammed362@gmail.com>
2023-12-05 22:43:06 +0000

    hello

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This is a module that writes a string to a
-text file ad return the number of """
+"""This module defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """This function writes a string to the file and
-    count the number of characters"""
-
-    with open(filename, "w", encoding="uft-8") as f:
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
