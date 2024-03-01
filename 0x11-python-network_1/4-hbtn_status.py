f69049b61f81eaaae1ce96b11544d0ad1e4fcc19
basit-devBE <basitmohammed362@gmail.com>
2024-03-01 16:02:54 +0000

    hello:

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
+    
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,3 +12,4 @@ if __name__ == "__main__":
 
     r = requests.post(url, data=value)
     print(r.text)
+    
