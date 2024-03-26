76b0c79b9d1fbba9683fb786953c12d13b10084d
basit-devBE <basitmohammed362@gmail.com>
2024-03-26 21:39:51 +0000

    hello

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-    This is a simple script that writes a string to a file. 
+    This is a simple script that writes a string to a file.
 */
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
