2cdc8a4dc3a8c7ed398949a39985dae3785b219d
basit-devBE <basitmohammed362@gmail.com>
2024-03-27 12:40:43 +0000

    error fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,12 @@
 #!/usr/bin/node
-/**
- * A script that prints the number of movies where
- *  the character "Wedge Antilles" is present.
-*/
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
-let count = 0;
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const films = JSON.parse(body).results;
-    for (const film of films) {
-      for (const character of film.characters) {
-        if (character.includes('18')) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
