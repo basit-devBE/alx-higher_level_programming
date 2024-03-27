37d4c65f4664f57f8c8d09910777a4de4362779e
basit-devBE <basitmohammed362@gmail.com>
2024-03-27 13:09:59 +0000

    hi

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,26 @@
 #!/usr/bin/node
-/**
- * A script that prints the number of tasks completed by a user.
- * The user ID matches a given integer.
- */
+
 const request = require('request');
-const userId = process.argv[2];
-const url = 'https://jsonplaceholder.typicode.com/todos';
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
     const tasks = JSON.parse(body);
-    const completedTasks = tasks.filter(task => task.userId === parseInt(userId) && task.completed);
-    console.log(completedTasks.length);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
