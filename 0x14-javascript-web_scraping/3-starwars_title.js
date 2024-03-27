f9bfdb94713496cc78c274524f8d4b73e175c071
basit-devBE <basitmohammed362@gmail.com>
2024-03-27 12:22:48 +0000

    hello

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@
  */
 const request = require('request');
 const episode = process.argv[2];
-const url= 'https://swapi-api.hbtn.io/api/films/'
+const url = 'https://swapi-api.hbtn.io/api/films/';
 request(url + episode, function (error, response, body) {
   if (error) {
     console.log(error);
